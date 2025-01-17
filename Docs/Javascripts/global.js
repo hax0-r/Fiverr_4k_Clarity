107ce8c281e35a74da4e5d843a9f6deeaa5affea
Muhammad <talha18513@gmail.com>
2025-01-17 16:05:17 +0500

    website done

--- Docs/Javascripts/global.js
+++ Docs/Javascripts/global.js
@@ -9,9 +9,25 @@ const responsiveNavbar = document.getElementById("responsiveNavbar")
 const closeNav = document.getElementById("closeNav")
 const openNav = document.getElementById("openNav")
 
-openNav.addEventListener('click',()=>{
+openNav.addEventListener('click', () => {
     responsiveNavbar.style.width = "100%"
 })
-closeNav.addEventListener('click',()=>{
+closeNav.addEventListener('click', () => {
     responsiveNavbar.style.width = "0%"
 })
+
+const openManageConsent = document.getElementById("openManageConsent")
+const ManageConsent = document.getElementById("ManageConsent")
+const closeManageConsent = document.getElementsByClassName("closeManageConsent")
+
+openManageConsent.addEventListener('click', () => {
+    ManageConsent.classList.remove("hidden")
+    openManageConsent.classList.add("hidden")
+})
+
+Array.from(closeManageConsent).forEach(button => {
+    button.addEventListener('click', () => {
+        ManageConsent.classList.add("hidden");
+        openManageConsent.classList.remove("hidden");
+    });
+});
